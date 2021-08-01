1a98f9eb24f6e04a9a8f26c88930330543c682fc
Artem Zyrianov <mister.zyrianovartyom@yandex.ru>
2021-08-02 01:15:24 +0600

    Добавил мидлвару авторизации и хелмет

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const auth = require('./middlewares/auth');
 const userRoutes = require('./routes/users');
 const movieRoutes = require('./routes/movies');
+const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -13,6 +16,12 @@ mongoose.connect('mongodb://localhost:27017/movies-explorer', {
   useFindAndModify: false,
 });
 
+app.use(helmet());
+
+app.post('/signup', createUser);
+app.post('/signin', login);
+
+app.use(auth);
 app.use('/users', userRoutes);
 app.use('/movies', movieRoutes);
 
---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const AutorizationError = require('../errors/authorization-err');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new AutorizationError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+      { expiresIn: '7d' });
+  } catch (error) {
+    next(new AutorizationError('Необходима авторизация'));
+  }
+  req.user = payload;
+
+  next();
+};
