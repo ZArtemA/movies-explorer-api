25f81458a1f0eae67f8403184db064b7550b9218
Artem Zyrianov <mister.zyrianovartyom@yandex.ru>
2021-08-02 03:09:55 +0600

    Поправил апп и добавил ошибки страницы и сервера

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const userRoutes = require('./routes/users');
 const movieRoutes = require('./routes/movies');
 const { login, createUser } = require('./controllers/users');
+const errorsHandler = require('./middlewares/errorsHandler');
+const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -16,14 +22,42 @@ mongoose.connect('mongodb://localhost:27017/movies-explorer', {
   useFindAndModify: false,
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(cors());
 
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}),
+login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(40),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string()
+      .pattern(/^(http:|https:)\/\/(w{3}\.)?[^а-яё\s]*$/)
+      .min(2)
+      .max(80),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}),
+createUser);
 
-app.use(auth);
-app.use('/users', userRoutes);
-app.use('/movies', movieRoutes);
+app.use(cookieParser());
+
+app.use('/users', auth, userRoutes);
+app.use('/movies', auth, movieRoutes);
+
+app.use('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+app.use(errors());
+app.use(errorsHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
